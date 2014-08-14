4ff677c91145e3ea256bb28e95ce6d853848b728
Sabbir Ahmed <sabbiirr@gmail.com>
2014-08-15 01:02:14 +0600

    Metods

--- pyt.py
+++ pyt.py
@@ -13,8 +13,26 @@ db = SQLAlchemy(app)
 class Member(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    finance = relationship('Finance', backref='by', lazy='dynamic')
+    balances = relationship('Finance', backref='by', lazy='dynamic')
     meals = relationship('Meal', backref='meals', lazy='dynamic')
+
+    def total_debit(self):
+        total_debit = 0
+        for balance in self.balances:
+            total_debit += balance.debit
+        return total_debit
+
+    def total_credit(self):
+        total_credit = 0
+        for balance in self.balances:
+            total_credit += balance.credit
+        return total_credit
+
+    def total_meal(self):
+        total_meal = 0
+        for meal in self.meals:
+            total_meal += meal.number
+        return total_meal
 
 
 class Meal(db.Model):
@@ -33,13 +51,16 @@ class Finance(db.Model):
     member = Column(String, ForeignKey('member.name'))
     notes = Column(Text)
 
+
 db.create_all()
 
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
 
-api_manager.create_api(Member, methods=['GET', 'POST', 'PUT', 'DELETE'])
+api_manager.create_api(Member, methods=['GET', 'POST', 'PUT', 'DELETE'],
+                       exclude_columns=['meals', 'balances'],
+                       include_methods=['total_debit', 'total_credit', 'total_meal'])
 
-api_manager.create_api(Meal, methods=['GET', 'POST', 'PUT', 'DELETE'],  exclude_columns=['meals'])
+api_manager.create_api(Meal, methods=['GET', 'POST', 'PUT', 'DELETE'])
 
 api_manager.create_api(Finance, methods=['GET', 'POST', 'PUT', 'DELETE'])
 
