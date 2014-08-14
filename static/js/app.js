3686d3a1f2861366d07805c9d9321afe53ed65dc
Sabbir Ahmed <sabbiirr@gmail.com>
2014-08-14 17:28:56 +0600

    Meals Implementation

--- static/js/app.js
+++ static/js/app.js
@@ -5,28 +5,37 @@ App.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 
     $routeProvider
         .when('/', {
-            controller : 'Members',
-            templateUrl : 'templates/members.html'
+            controller: 'Members',
+            templateUrl: 'templates/members.html'
+        })
+        .when('/meals', {
+            controller: 'Meals',
+            templateUrl: 'templates/meals.html'
         })
         .otherwise({
             redirectTo: '/'
-         })
+        })
 
 
 }]);
 
 App.controller('AppCtrl', ['$scope', function ($scope) {
     $scope.sortBy = function (value) {
-         $scope.reverse = !$scope.reverse;
-         $scope.order = value;
-      };
+        $scope.reverse = !$scope.reverse;
+        $scope.order = value;
+    };
 }]);
 
 App.controller('Members', ['$scope', '$http', function ($scope, $http) {
-    $http.get('/api/member').success(function (data) {
-         $http.get("/api/member").success(function (data) {
-           $scope.members = data.objects;
-        });
+    $http.get("/api/member").success(function (data) {
+        $scope.members = data.objects;
     });
 }]);
 
+
+App.controller('Meals', ['$scope', '$http', function ($scope, $http) {
+    $http.get("/api/meal").success(function (data) {
+        $scope.meals = data.objects;
+        console.log(data.objects);
+    });
+}]);
