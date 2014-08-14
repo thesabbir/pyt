a10a32202286434e28237777d7e927d11d5a8078
Sabbir Ahmed <sabbiirr@gmail.com>
2014-08-15 02:19:19 +0600

    Manger Completed

--- pyt.py
+++ pyt.py
@@ -53,24 +53,59 @@ class Balance(db.Model):
     manager = Column(String, ForeignKey('manager.id'), default=0)
     notes = Column(Text)
 
+
 class Manager(db.Model):
     id = Column(Integer, primary_key=True)
+    name = Column(String, default="Fayez Bhaban")
     balances = relationship('Balance', backref='balances', lazy='dynamic')
     meals = relationship('Meal', backref='meals', lazy='dynamic')
 
+    def total_debit(self):
+        total_debit = 0
+        for balance in self.balances:
+            total_debit += balance.debit
+        return total_debit
+
+    def total_credit(self):
+        total_credit = 0
+        for balance in self.balances:
+            total_credit += balance.credit
+        return total_credit
+
+    def total_meal(self):
+        total_meal = 0
+        for meal in self.meals:
+            total_meal += meal.number
+        return total_meal
+
+    def meal_rate(self):
+        meal_rate = self.total_credit() / self.total_meal()
+        return  meal_rate
 
 db.create_all()
 
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
 
-api_manager.create_api(Member, methods=['GET', 'POST', 'PUT', 'DELETE'],
-                       exclude_columns=['meals', 'balances'],
-                       include_methods=['total_debit', 'total_credit', 'total_meal'])
+api_manager.create_api(
+    Member, methods=['GET', 'POST', 'PUT', 'DELETE'],
+    exclude_columns=['meals', 'balances'],
+    include_methods=['total_debit', 'total_credit', 'total_meal']
+)
 
-api_manager.create_api(Meal, methods=['GET', 'POST', 'PUT', 'DELETE'], exclude_columns=['by'])
+api_manager.create_api(
+    Meal, methods=['GET', 'POST', 'PUT', 'DELETE'],
+    exclude_columns=['by']
+)
 
-api_manager.create_api(Balance, methods=['GET', 'POST', 'PUT', 'DELETE'])
-api_manager.create_api(Manager, methods=['GET'])
+api_manager.create_api(
+    Balance, methods=['GET', 'POST', 'PUT', 'DELETE']
+)
+
+api_manager.create_api(
+    Manager, methods=['GET'],
+    exclude_columns=['meals', 'balances'],
+    include_methods=['total_debit', 'total_credit', 'total_meal', 'meal_rate']
+)
 
 
 @app.route('/')
--- static/js/app.js
+++ static/js/app.js
@@ -12,9 +12,13 @@ App.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
             controller: 'MealCtrl',
             templateUrl: 'templates/meals.html'
         })
-         .when('/balances', {
+        .when('/balances', {
             controller: 'BalanceCtrl',
             templateUrl: 'templates/balances.html'
+        })
+        .when('/manager', {
+            controller: 'ManageCtrl',
+            templateUrl: 'templates/manager.html'
         })
         .otherwise({
             redirectTo: '/'
@@ -30,17 +34,22 @@ App.controller('AppCtrl', ['$scope', function ($scope) {
     };
 }])
     .controller('MemberCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http.get("/api/member").success(function (data) {
-        $scope.members = data.objects;
-    });
-}])
+        $http.get("/api/member").success(function (data) {
+            $scope.members = data.objects;
+        });
+    }])
     .controller('MealCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http.get("/api/meal").success(function (data) {
-        $scope.meals = data.objects;
-    });
-}])
+        $http.get("/api/meal").success(function (data) {
+            $scope.meals = data.objects;
+        });
+    }])
     .controller('BalanceCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http.get("/api/balance").success(function (data) {
-        $scope.balances = data.objects;
-    });
-}])
+        $http.get("/api/balance").success(function (data) {
+            $scope.balances = data.objects;
+        });
+    }])
+    .controller('ManageCtrl', ['$scope', '$http', function ($scope, $http) {
+        $http.get("/api/manager").success(function (data) {
+            $scope.managers = data.objects;
+        })
+    }]);
